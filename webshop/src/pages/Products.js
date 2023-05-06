fb060af6a8ab4c2c133c69133802a4f1305f7191
fzszabo <szaboflorazita@gmail.com>
2023-05-06 11:03:54 +0200

    create products

--- webshop/src/pages/Products.js
+++ webshop/src/pages/Products.js
@@ -1,7 +1,21 @@
+import productService from "../service/productService"
+
 const Products = () => {
+
+    function createProducts() {
+        const product = {
+            title: prompt("Adj meg egy nevet!"),
+            price: prompt("Adj meg egy árat!"),
+        }
+
+        productService.create(product)
+            .then(product => console.log(product))
+    }
+
     return (
         <>
             <p>Termékek</p>
+            <button onClick={createProducts}>Termék hozzáadás</button>
         </>
     )
 }
---
+++ webshop/src/service/productService.js
@@ -0,0 +1,16 @@
+const API_URL = 'https://csapat-07-default-rtdb.europe-west1.firebasedatabase.app/'
+
+function create(product) {
+    return fetch(`${API_URL}products.json`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(product)
+    })
+    .then(res => res.json())
+}
+
+export default {
+    create: create
+}
