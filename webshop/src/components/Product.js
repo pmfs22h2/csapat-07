7cf474ad7bffeef8fc3643eaff952a21c18f6e31
kemenykin <kemenykin@gmail.com>
2023-05-09 13:20:38 +0200

    products grid basics - need to fix

--- webshop/src/components/Product.js
+++ webshop/src/components/Product.js
@@ -1,8 +1,12 @@
+import '../styles/products.css';
+
 export default function Products(props) {
     return (
-        <div>
-            <h2>Termék neve: {props.product.title}</h2>
-            <p>Ár: {props.product.price}</p>
+        <div className="product-card">
+            <div>
+                <h4>Termék neve: {props.product.title}</h4>
+                <p>Ár: {props.product.price}</p>
+            </div>
         </div>
     )
 }
