beb25a0089fb8a77cc73fe3db60714d930b86f53
kemenykin <kemenykin@gmail.com>
2023-05-09 19:07:57 +0200

    basic product card css added

--- webshop/src/components/Product.js
+++ webshop/src/components/Product.js
@@ -2,11 +2,12 @@ import '../styles/products.css';
 
 export default function Products(props) {
     return (
-        <div className="product-card">
-            <div>
-                <h4>Termék neve: {props.product.title}</h4>
+        <>
+            <div className="product" style={{backgroundImage: `url(https://images.pexels.com/photos/4938502/pexels-photo-4938502.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+                <h4>Termék neve:</h4>
+                <p>{props.product.title}</p>
                 <p>Ár: {props.product.price}</p>
             </div>
-        </div>
+        </>
     )
 }
