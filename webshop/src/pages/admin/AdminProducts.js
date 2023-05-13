546b9f00181fc6e1687ba929ca6357314f25bc0e
dtmelinda <dudas.toth.melinda@gmail.com>
2023-05-13 16:00:52 +0200

    sort and turn done

--- webshop/src/pages/admin/AdminProducts.js
+++ webshop/src/pages/admin/AdminProducts.js
@@ -1,7 +1,5 @@
 import productService from "../../../src/service/productService"
 import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-//import ProductList from "../../components/user/ProductList";
 import AdminProductList from "../../components/admin/AdminProductList";
 import SearchComponent from "../../components/user/SearchComponent";
 import sortProductsFromA from "../../utils/sortProductsFromA";
@@ -10,41 +8,22 @@ import sortProductsFromB from "../../utils/sortProductsFromB";
 const AdminProducts = () => {
 
     const [products, setProducts] = useState([]);
-    // const [searchParams, setSearchParams] = useSearchParams();
     const [from, setFrom] = useState(0);
     const [to, setTo] = useState(9);
     const [displayedProducts, setDisplayedProducts] = useState([]);
     const [selectValue, setSelectValue] = useState("order");
-
-    // const [sortByTitle, setSortByTitle] = useState({ sort: searchParams.get("sort") || "" });
-
     const [sortedItems, setSortedItems] = useState();   // sortedItems és setSortedItems kell? nem használjuk sehol
-
+    
     useEffect(() => {
         listProducts();
-        if (selectValue === "name-asc" || selectValue === "") {
-            setSortedItems(sortProductsFromA(products))
-        }
-    }, [])
-
-    useEffect(() => {
-        listProducts();
-        if (selectValue === "name-desc" || selectValue === "") {
-            setSortedItems(sortProductsFromB(products))
-        }
     }, [])
 
     function listProducts() {
         productService.read()
             .then(product => {
                 let manipulatedProducts = productService.manipulateProductObject(product);
-                if (selectValue === "name-asc") {
-                    manipulatedProducts.sort((a, b) => a.title.localeCompare(b.title, 'hu-HU'));
-                }
-                if (selectValue === "name-desc") {
-                    manipulatedProducts.sort((a, b) => b.title.localeCompare(a.title, 'hu-HU'));
-                }
                 setProducts(manipulatedProducts);
+                setSortedItems(manipulatedProducts)
                 const manProdLenght = manipulatedProducts.length;
 
                 if (manProdLenght < to) {
@@ -62,75 +41,56 @@ const AdminProducts = () => {
 
         setFrom(decreasedFrom);
         setTo(decreasedTo);
-        setDisplayedProducts(products.slice(decreasedFrom, decreasedTo));
+        setDisplayedProducts(sortedItems.slice(decreasedFrom, decreasedTo));
     }
 
     function nextPage() {
+        console.log("next");
         let increasedFrom = from + 9;
-        let increasedTo = products.length >= to + 9 ? to + 9 : products.length;
+        let increasedTo = sortedItems.length >= to + 9 ? to + 9 : sortedItems.length;
 
         setFrom(increasedFrom);
         setTo(increasedTo);
-        setDisplayedProducts(products.slice(increasedFrom, increasedTo));
+        setDisplayedProducts(sortedItems.slice(increasedFrom, increasedTo));
     }
 
-    // function handleSortButtonOnClickAsc() {
-    //     setSearchParams({ "sort": "asc" })
-    //     console.log(sortByTitle.sort)
-    //     if (sortByTitle.sort === "asc" || sortByTitle.sort === "") {
-    //         setSortedItems(sortProductsFromA(displayedProducts))
-    //     }
-    //     setDisplayedProducts(sortProductsFromA)
-    // }
-
-    // function handleSortButtonOnClickDesc() {
-    //     setSearchParams({ "sort": "desc" })
-    //     console.log(sortByTitle.sort)
-    //     if (sortByTitle.sort === "desc" || sortByTitle.sort === "") {
-    //         setSortedItems(sortProductsFromB(displayedProducts))
-    //     }
-    //     setDisplayedProducts(sortProductsFromB)
-    // }
+    function sliceprod(array) {
+        let increasedFrom = 0;
+        let increasedTo = 9;
+        setFrom(increasedFrom);
+        setTo(increasedTo);
+        setDisplayedProducts(array.slice(increasedFrom, increasedTo));
+    }
 
     useEffect(() => {
         if (selectValue === "name-desc") {
+            const prod = sortProductsFromB(products)
             setSortedItems(sortProductsFromB(products))
+            sliceprod(prod)
+
         } else if (selectValue === "name-asc") {
-                setSortedItems(sortProductsFromA(products))
+            const prod = sortProductsFromA(products)
+            setSortedItems(sortProductsFromA(products))
+            sliceprod(prod)
+        } else {
+            setSortedItems(products)
         }
-    }, [selectValue])
+    }, [selectValue]);
 
-    useEffect(() => {
-        productService.read()
-            .then(item => {
-                let manipulatedProducts = productService.manipulateProductObject(item);
-                setProducts(manipulatedProducts)
-            });
-
-    }, [])
-
-    console.log(sortedItems);
-    console.log(selectValue);
     return (
         <>
             <select value={selectValue} id="ordered-list" onChange={(e) => setSelectValue(e.target.value)} >
                 <option value="order">Rendezés</option>
-                <option value="name-asc" >Név szerint növekvő</option>
-                <option value="name-desc" >Név szerint csökkenő</option>
+                <option value="name-asc">Név szerint növekvő</option>
+                <option value="name-desc">Név szerint csökkenő</option>
                 <option value="price-asc">Ár szerint növekvő</option>
                 <option value="price-desc">Ár szerint csökkenő</option>
             </select>
-            {/* <button onClick={handleSortButtonOnClickAsc}>
-                Rendezés A-Z
-            </button>
-            <button onClick={handleSortButtonOnClickDesc}>
-                Rendezés Z-A
-            </button> */}
             <p>Admin termék lista</p>
             <SearchComponent products={displayedProducts} />
-            <AdminProductList products={sortedItems} />
+            <AdminProductList products={displayedProducts} />
             <div className="pagination-buttons">
-                <button onClick={prevPage} disabled={from === 0 }>Vissza</button>
+                <button onClick={prevPage} disabled={from === 0}>Vissza</button>
                 <button onClick={nextPage} disabled={to === products.length}>Előre</button>
             </div>
         </>
