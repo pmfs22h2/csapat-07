d6822566916eec699bec1360a4c0bbc50224599a
dtmelinda <dudas.toth.melinda@gmail.com>
2023-05-16 22:15:48 +0200

    admin user list done

--- webshop/src/components/admin/UserData.js
+++ webshop/src/components/admin/UserData.js
@@ -1,12 +1,11 @@
 import '../../styles/admin.css';
 
-
 export default function UserData(props) {
     return (
         <tr>
             <td>{props.user.uid}</td>
             <td>{props.user.name}</td>
-            {/* <td>{props.user.email}</td> */}
+            <td>{props.user.email}</td>
         </tr>
     )
 }
