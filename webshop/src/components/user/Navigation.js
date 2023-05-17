44742cd6e7a1c6c751d0865663df530e05e78fae
kemenykin <kemenykin@gmail.com>
2023-05-17 15:44:10 +0200

    nav icons added

--- webshop/src/components/user/Navigation.js
+++ webshop/src/components/user/Navigation.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import '../../styles/navigation.css';
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+import { FaUserAlt, FaShoppingBag, FaHeart } from "react-icons/fa";
 
 const Navigation = () => {
     const { userData, setUserData } = useContext(AuthContext);
@@ -11,14 +12,19 @@ const Navigation = () => {
             <div className='navbar'>
                 <Link to='/'>Kezdőoldal</Link>
                 <Link to='/termekek'>Termékek</Link>
-                {!userData ? <Link to='/admin'>Admin</Link> : <></>} 
+                {!userData ? <Link to='/admin'>Admin</Link> : <></>}
                 <Link to='/kosar'>Kosár</Link>
-                {!userData ? <Link to='/regisztracio'>Regisztráció</Link> : <></>} 
+                {!userData ? <Link to='/regisztracio'>Regisztráció</Link> : <></>}
                 {!userData ? <Link to='/belepes'>Bejelentkezés</Link> : <></>}
             </div>
             <div className='user-login'>
                 {userData ? `Bejelentkezve, mint: ${userData.name}` : "Még nem vagy bejelentkezve"}
                 {userData ? <button onClick={() => setUserData(null)}>Kijelentkezés</button> : <></>}</div>
+            <div className='nav-icons'>
+                <FaUserAlt />
+                <FaHeart />
+                <FaShoppingBag />
+            </div>
         </div>
     )
 }
