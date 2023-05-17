dfb294e749c65d44236eda2a3bc0cbf9d50988f0
kemenykin <kemenykin@gmail.com>
2023-05-17 18:14:03 +0200

    search

--- webshop/src/components/user/SearchComponent.js
+++ webshop/src/components/user/SearchComponent.js
@@ -4,29 +4,42 @@ const SearchComponent = (props) => {
 
     const [filteredProducts, setFilteredProducts] = useState(null);
 
-    const [searcValue, setSearchValue] = useState('');
+    const [searchValue, setSearchValue] = useState('');
 
     const searchedProduct = (e) => {
         setSearchValue(e.target.value);
-        console.log(searcValue);
+        console.log(searchValue);
     }
 
     const filter = () => {
-        const filteredProductsArray = props.products.filter(product => product.title.includes(searcValue));
+        const filteredProductsArray = props.products.filter(product => product.title.toLowerCase().includes(searchValue));
         setFilteredProducts(filteredProductsArray);
         console.log(filteredProducts);
+    }
+
+    const deleteSearchProducts = () => {
+        setFilteredProducts(null);
+        setSearchValue("");
     }
 
     return (
         <div>
             <div>
-                Keresés: <input type="text" onChange={searchedProduct} value={searcValue} />
+                Keresés: <input type="text" onChange={searchedProduct} value={searchValue} />
                 <button onClick={filter}>Keresés</button>
+                <button onclick={deleteSearchProducts}>x</button>
             </div>
 
-
             <div>
-                Találatok: {filteredProducts && filteredProducts.map(products => <div key={crypto.randomUUID()}>{products.title}</div>)}
+                {
+                    filteredProducts && (
+                        filteredProducts?.length !== 0
+                            ?
+                            <><h1>Találatok:</h1> {filteredProducts?.map(products => <div key={crypto.randomUUID()}>{products.title}</div>)}</>
+                            :
+                            "nincs ilyen termék"
+                    )
+                }
             </div>
         </div>
     )
