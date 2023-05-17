fe667597bf897a3edff7302a1e56dec6fe2f7ee1
kemenykin <kemenykin@gmail.com>
2023-05-17 13:17:03 +0200

    debug console errors - classes to className

--- webshop/src/components/user/Footer.js
+++ webshop/src/components/user/Footer.js
@@ -2,18 +2,18 @@ import "../../styles/footer.css";
 
 const Footer = () => {
     return (
-        // <footer className="footer">
-        //     <div className="footer-container">
+        // <footer classNameName="footer">
+        //     <div classNameName="footer-container">
         //     <p>Sample text</p>
         //     <p>Networking links</p>
         //     <p>Icons</p>
         //     </div>
         // </footer>
 
-<footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
+<footer className="footer">
+  	 <div className="container">
+  	 	<div className="row">
+  	 		<div className="footer-col">
   	 			<h4>company</h4>
   	 			<ul>
   	 				<li><a href="#">about us</a></li>
@@ -22,7 +22,7 @@ const Footer = () => {
   	 				<li><a href="#">affiliate program</a></li>
   	 			</ul>
   	 		</div>
-  	 		<div class="footer-col">
+  	 		<div className="footer-col">
   	 			<h4>online shop</h4>
   	 			<ul>
   	 				<li><a href="#">watch</a></li>
@@ -31,13 +31,13 @@ const Footer = () => {
   	 				<li><a href="#">dress</a></li>
   	 			</ul>
   	 		</div>
-               <div class="footer-col">
+               <div className="footer-col">
   	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
+  	 			<div className="social-links">
+  	 				<a href="#"><i className="fab fa-facebook-f"></i></a>
+  	 				<a href="#"><i className="fab fa-twitter"></i></a>
+  	 				<a href="#"><i className="fab fa-instagram"></i></a>
+  	 				<a href="#"><i className="fab fa-linkedin-in"></i></a>
   	 			</div>
   	 		</div>
   	 	</div>
