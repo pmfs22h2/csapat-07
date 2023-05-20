e5f78be00ddcb6b26ae0f04853a53ed0d0dfaea8
dtmelinda <dudas.toth.melinda@gmail.com>
2023-05-20 18:42:57 +0200

    admin user sort and turn with mistake

--- webshop/src/pages/admin/AdminUserList.js
+++ webshop/src/pages/admin/AdminUserList.js
@@ -1,23 +1,104 @@
 import { useEffect, useState } from "react";
-import userService from "../../service/userService";
 import UserList from "../../components/admin/UserList";
 import AdminUserSearchComponent from "../../components/admin/AdminUserSearchComponent";
+import sortUsersFromA from "../../utils/sortUsersFromA";
+import sortUsersFromB from "../../utils/sortUsersFromB";
+import userService from "../../service/userService";
 
 const AdminUserList = () => {
 
-    const [webshopUsers, setWebshopUsers] = useState();
+    const [webshopUsers, setWebshopUsers] = useState([]);
+    const [from, setFrom] = useState(0);
+    const [to, setTo] = useState(9);
+    const [displayedUsers, setdisplayedUsers] = useState([]);
+    const [selectValue, setSelectValue] = useState("order");
+    const [sortedItems, setSortedItems] = useState();
+
+    useEffect(() => {
+        listUsers();
+    }, [])
 
     useEffect(() => {
         userService.read()
             .then(users => setWebshopUsers(users))
     }, [])
-    console.log(webshopUsers)
+
+    function listUsers() {
+        userService.read()
+            .then(users => {
+                let manipulatedUsers = userService.manipulateUserObject(users);
+                setWebshopUsers(manipulatedUsers);
+                setSortedItems(manipulatedUsers)
+                const manUsLength = manipulatedUsers.length;
+
+                if (manUsLength < to) {
+                    setTo(manUsLength);
+                    setdisplayedUsers(manipulatedUsers);
+                } else {
+                    setdisplayedUsers(manipulatedUsers.slice(from, to));
+                }
+            })
+    }
+
+    function prevPage() {
+        let decreasedFrom = from - 9;
+        let decreasedTo = to % 9 === 0 ? to - 9 : to - (to % 9);
+
+        setFrom(decreasedFrom);
+        setTo(decreasedTo);
+        setdisplayedUsers(sortedItems.slice(decreasedFrom, decreasedTo));
+    }
+
+    function nextPage() {
+        console.log("next");
+        let increasedFrom = from + 9;
+        let increasedTo = sortedItems.length >= to + 9 ? to + 9 : sortedItems.length;
+
+        setFrom(increasedFrom);
+        setTo(increasedTo);
+        setdisplayedUsers(sortedItems.slice(increasedFrom, increasedTo));
+    }
+
+    function sliceprod(array) {
+        let increasedFrom = 0;
+        let increasedTo = 9;
+        setFrom(increasedFrom);
+        setTo(increasedTo);
+        setdisplayedUsers(array.slice(increasedFrom, increasedTo));
+    }
+
+    useEffect(() => {
+        if (selectValue === "name-desc") {
+            const prod = sortUsersFromB(webshopUsers)
+            setSortedItems(sortUsersFromB(webshopUsers))
+            sliceprod(prod)
+
+        } else if (selectValue === "name-asc") {
+            const prod = sortUsersFromA(webshopUsers)
+            setSortedItems(sortUsersFromA(webshopUsers))
+            sliceprod(prod)
+
+        } else {
+            setSortedItems(webshopUsers)
+        }
+    }, [selectValue]);
+console.log(webshopUsers)
+
 
     return (
         <>
+            <select value={selectValue} id="ordered-list" onChange={(e) => setSelectValue(e.target.value)} >
+                <option value="order">Rendezés</option>
+                <option value="name-asc">Név szerint növekvő</option>
+                <option value="name-desc">Név szerint csökkenő</option>
+            </select>
             <p>Admin felhasználók lista</p>
-            <AdminUserSearchComponent users={webshopUsers} />
-            <UserList users={webshopUsers}/>
+            <AdminUserSearchComponent users={displayedUsers} />
+            <UserList users={displayedUsers} />
+            <div className="pagination-buttons">
+                <button onClick={prevPage} disabled={from === 0}>Vissza</button>
+                <button onClick={nextPage} disabled={to === webshopUsers.length}>Előre</button>
+            </div>
         </>
     )
 }
