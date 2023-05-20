96b478552ce663aabe2f9fb794010e24f4e4ddc2
livia-varga <varga.livia86@gmail.com>
2023-05-20 09:19:38 +0200

    admin nav css

--- webshop/src/components/admin/AdminNavigation.js
+++ webshop/src/components/admin/AdminNavigation.js
@@ -3,12 +3,14 @@ import '../../styles/navigation.css';
 
 const AdminNavigation = () => {
     return (
-        <div className='navbar'>
-            <Link to='/admin'>Admin</Link>
-            <Link to='/admin/termekek'>Termék lista</Link>
-            <Link to='/admin/termek-felvitel'>Termékfelvitel</Link>
-            <Link to='/'>Vissza</Link>
-            <Link to='/admin/vasarlok'>Felhasználók lista</Link>
+        <div className='header-container'>
+            <div className='navbar'>
+                <Link to='/admin'>Admin</Link>
+                <Link to='/admin/termekek'>Termék lista</Link>
+                <Link to='/admin/termek-felvitel'>Termékfelvitel</Link>
+                <Link to='/'>Vissza</Link>
+                <Link to='/admin/vasarlok'>Felhasználók lista</Link>
+            </div>
         </div>
     )
 }
