fd25043af2fcfeead45327021d67883aacfc23ef
livia-varga <varga.livia86@gmail.com>
2023-05-20 07:06:24 +0200

    regisztracio css

--- webshop/src/components/user/Registration.js
+++ webshop/src/components/user/Registration.js
@@ -64,7 +64,7 @@ export default function Registration() {
                     />
                     <label> Jelszó: </label>
                 </p>
-                <p><button className = "reg-button" type="submit" onClick={register}>küldés</button></p>
+                <p><button className = "reg-button" type="submit" onClick={register}>Küldés</button></p>
             </form>
             {errorMsg && <div>{errorMsg}</div>}
             </div>
