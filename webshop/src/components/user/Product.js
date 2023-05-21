7f806bdf4859f7bffebb9f6536fb5f90bdaae9c7
kemenykin <kemenykin@gmail.com>
2023-05-21 20:24:39 +0200

    add to cart w/ login error msg

--- webshop/src/components/user/Product.js
+++ webshop/src/components/user/Product.js
@@ -4,6 +4,8 @@ import { AuthContext } from '../../context/AuthContext';
 import { CartContext } from '../../context/cartContext';
 import getCartList from '../../utils/getCartList';
 import cartService from '../../service/cartService';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Products(props) {
 
@@ -13,7 +15,9 @@ export default function Products(props) {
 
     function addItemToCart() {
         if (userData === null) {
-            alert("Nem vagy bejelentkezve!");
+            toast.error('Jelentkezz be a vásárláshoz!', {
+                position: toast.POSITION.BOTTOM_CENTER
+            });
             return;
         }
         cartService.addToCart(props.product.id, userData.uid);
@@ -30,8 +34,8 @@ export default function Products(props) {
                     <div className='product-title'>{props.product.title}</div>
                     <div className='product-price'>{props.product.price} HUF</div>
                     <button className="cart-button" onClick={addItemToCart}>Kosárba</button>
-                    {/* Innen hiányzik még a feltételes megjelenítés, ha nincs bejelentkezve és úgy akar a kosárba tenni! */}
                     {isAddedToCart && <div className='added-success'>A termék bekerült a kosárba!</div>}
+                    <ToastContainer />
                 </div>
             </div>
         </>
