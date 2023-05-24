a2ed332ae6bb6057a74cc93ba04702fd15443dc9
livia-varga <varga.livia86@gmail.com>
2023-05-24 18:52:29 +0200

    changes

--- webshop/src/components/user/CategorySearch.js
+++ webshop/src/components/user/CategorySearch.js
@@ -1,31 +1,34 @@
 import { useEffect, useState } from "react";
 import categoryService from "../../service/categoryService";
+import AdminAddProduct from "../../pages/admin/AdminAddProduct";
 
 
 
 export default function CategorySearch() {
 
-    // const [categoryName, setCategoryName] = useState("");
-    // const [categoryData, setCategoryData] = useState({});
+    const [categoryName, setCategoryName] = useState("");
+    const [categoryData, setCategoryData] = useState({});
 
-    // const [selectedCategory, setSelectedCategory] = useState();
+    const [selectedCategory, setSelectedCategory] = useState();
 
-    // useEffect(() => {
-    //     categoryService.readCategories()
-    //         .then(json => {
-    //             setCategoryData(Object.values(json))
-    // })
-    // }, []);
-  
+
+    useEffect(() => {
+        categoryService.readCategories()
+            .then(json => {
+                setCategoryData(Object.values(json))
+    })
+    }, []);
 
     return (
         <div>
+            <AdminAddProduct/>
             <div className="sort-menu">
-                    <select>
-                        <option value="Kategória">Kategória szerinti szűrés</option>
-                        <option value="">Összes kategória</option>
-                        <option value="">Kategória neve</option>
-                    </select>
+            <select>
+                <option value="">Válassz egy kategóriát!</option>
+                {Object.values(categoryData).map(cat => <option value={cat.id}>{cat.name}</option>)}
+                    <option value="">Összes kategória</option>
+                    <option value="">Kategória neve</option>
+            </select>
                 </div>
         </div>
     )
