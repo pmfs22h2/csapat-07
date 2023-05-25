70638698447b30758c5e94b3d3fc7ce273188da4
kemenykin <kemenykin@gmail.com>
2023-05-25 20:41:47 +0200

    intro page content added

--- webshop/src/components/user/ProductWizard/IntroPage.js
+++ webshop/src/components/user/ProductWizard/IntroPage.js
@@ -1,18 +1,23 @@
 import MainStepper from "./MainStepper";
 import { Link } from "react-router-dom";
+import '../../../styles/ProductWizard/intropage.css';
 
 const IntroPage = () => {
     return (
         <>
             <h2>Termék varázsló</h2>
-            <p>
-                Mit szólnál hozzá, ha egy gyors és interaktív kérdőív segítségével segítetnénk
-                neked megtalálni a számodra legmegfelelőbb terméket?
-            </p>
-            <p>
-                Mindössze annyi a dolgod, hogy válaszolsz 3 kérdésre, mi pedig kiválasztjuk a hozzád és
-                igényeidhez leginkább passzoló terméket kínálatunkból!
-            </p>
+            <p><img src="https://images.pexels.com/photos/6707334/pexels-photo-6707334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></p>
+            <div className="intro-container">
+                <p>
+                    Mit szólnál hozzá, ha egy gyors és interaktív kérdőív segítségével segítetnénk
+                    neked megtalálni a számodra legmegfelelőbb termékeket?
+                </p>
+                <p>
+                    Mindössze annyi a dolgod, hogy válaszolsz 3 kérdésre, mi pedig kiválasztjuk a hozzád és
+                    igényeidhez leginkább passzoló termékeket kínálatunkból!
+                </p>
+            </div>
+            <br />
             <Link to="/kerdesek">Induljon a varázslat!</Link>
         </>
     )
