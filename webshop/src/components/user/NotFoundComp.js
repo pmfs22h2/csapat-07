b397590d53901e58fa62d3bf306da037d4c9ea4d
kemenykin <kemenykin@gmail.com>
2023-05-25 16:39:12 +0200

    img added

--- webshop/src/components/user/NotFoundComp.js
+++ webshop/src/components/user/NotFoundComp.js
@@ -1,8 +1,11 @@
+import '../../styles/notfound.css';
+
 const NotFoundComp = () => {
     return (
         <div>
             <h1>Hoppá! Eltévedtél?</h1>
             <p>Az általad keresett oldal nem létezik.</p>
+            <img src="https://imagizer.imageshack.com/v2/798x356q90/922/PuFR3Q.png" alt=""/>
         </div>
     );
 }
