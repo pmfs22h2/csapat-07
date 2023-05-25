bba527bf0bdfb274058fa7c284878c8a0223d9e8
kemenykin <kemenykin@gmail.com>
2023-05-25 18:41:13 +0200

    delete toastcontainer

--- webshop/src/components/user/Registration.js
+++ webshop/src/components/user/Registration.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { registerUserAuth } from "../../service/auth-service";
 import "../../styles/registration.css";
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function Registration() {
