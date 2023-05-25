557f87988274e9fa1cfdddb43e26109fbfaef6c4
livia-varga <varga.livia86@gmail.com>
2023-05-25 22:59:46 +0200

    css fixing

--- webshop/src/components/user/UserProfile.js
+++ webshop/src/components/user/UserProfile.js
@@ -1,15 +1,20 @@
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
+import '../../styles/userprofile.css';
 
 const UserProfile = () => {
     const { userData } = useContext(AuthContext);
 
     return (
-        <div>
-            <h2>Felhasználói adatlap</h2>
-            <p>Név: {userData.name}</p>
-            <p>E-mail cím: {userData.email}</p>
+        <>
+        <h2 className="userprofile-h2">Felhasználói adatlap</h2>
+        <div className="user-profile">
+                <h3>Név:</h3>
+                <p>{userData.name}</p>
+                <h3>E-mail:</h3>
+                <p>{userData.email}</p>
         </div>
+        </>
     )
 }
 
