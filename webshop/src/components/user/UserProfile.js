41dbfcd41dc9af03baa12882a09258a7162b7df3
livia-varga <varga.livia86@gmail.com>
2023-05-26 00:19:16 +0200

    css fixing

--- webshop/src/components/user/UserProfile.js
+++ webshop/src/components/user/UserProfile.js
@@ -8,11 +8,32 @@ const UserProfile = () => {
     return (
         <>
         <h2 className="userprofile-h2">Felhasználói adatlap</h2>
+        <div className="hello">
+        <h3>Szia Liviteszt2!</h3>
+        <p>Örülünk , hogy újra benéztél hozzánk!</p>
+        </div>
         <div className="user-profile">
+            {/* <img src="../../styles/kep5.jpeg"/> */}
+            <div className="user-data">
                 <h3>Név:</h3>
                 <p>{userData.name}</p>
+                </div>
+                <div className="user-data">
                 <h3>E-mail:</h3>
                 <p>{userData.email}</p>
+                </div>
+                <div className="user-data">
+                <h3>Cím:</h3>
+                <p>1036 Budapest, Csemete utca 10.</p>
+                </div>
+                <div className="user-data">
+                <h3>Tel:</h3>
+                <p>+36 20 446 9194</p>
+                <div className="user-data">
+                <h3>Tagsági azonosítóm:</h3>
+                <p>123456789</p>
+                </div>
+                </div>    
         </div>
         </>
     )
