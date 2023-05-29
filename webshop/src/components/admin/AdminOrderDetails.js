09cc240d330961ac3fe641fa3c7d2de7876710a9
dtmelinda <dudas.toth.melinda@gmail.com>
2023-05-29 16:55:19 +0200

    WIP order details form

--- webshop/src/components/admin/AdminOrderDetails.js
+++ webshop/src/components/admin/AdminOrderDetails.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../styles/admin.css';
+import { useState } from "react";
 
 export default function AdminOrderDetails() {
+
+  const [displayedOrders, setDisplayedOrders] = useState([]);
+  const [users, setUsers] = useState([]);
+
   return (
     <>
-    <h2 className="admin-h2">AdminOrderDetails</h2>
-    <Link className="admin-back-btn" to='/admin/megrendelesek'>vissza</Link>
+      <h2 className="admin-h2">Megrendelés részletei</h2>
+      <table className="ordertable">
+        <thead>
+          <tr>
+            <th>Megrendelt termék</th>
+            <th>Termék mennyisége</th>
+            <th>Részösszeg</th>
+            {/* <th>Vásárló neve</th>
+            <th>Részletek</th> */}
+          </tr>
+        </thead>
+        <tbody>
+          {displayedOrders.map(order => (
+            <tr key={order.id}>
+              <td>{order.id}</td>
+              <td>{order.timestamp}</td>
+              <td>{order.uid}</td>
+              {/* <td>{users[order.uid]?.name}</td>
+              <td>
+                <Link to={`/admin/megrendelesek/${order.id}/adatlap`}>Adatlap</Link>
+              </td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Link className="admin-back-btn" to='/admin/megrendelesek'>Vissza</Link>
     </>
   )
 }
