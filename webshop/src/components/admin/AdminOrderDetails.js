9956ae2943c84aeb03eca5c86809d2144fabf749
Zsofi24 <zsofi24.hari.zsofi@gmail.com>
2023-05-29 22:28:42 +0200

    admin order details

--- webshop/src/components/admin/AdminOrderDetails.js
+++ webshop/src/components/admin/AdminOrderDetails.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import '../../styles/admin.css';
 import { useState } from "react";
+import orderService from '../../service/orderService';
+import userService from '../../service/userService';
+import getCartList from '../../utils/getCartList';
 
 export default function AdminOrderDetails() {
 
-  const [displayedOrders, setDisplayedOrders] = useState([]);
-  const [users, setUsers] = useState([]);
+// url-ből lekérjük a rendelés id-t
+  const { orderId } = useParams();
+  console.log(orderId, 'orderId');
+// a megrendelés
+  const [order, setOrder] = useState({});
+  console.log(order, 'order');
+// a megrendeléshez tartozó termékek
+  const [products, setProducts] = useState();
+  console.log(products, 'products');
+// a megrendelő neve
+  const [userName, setUserName] = useState("");
+  console.log(userName);
+
+  useEffect(() => {
+    // megrendelés id alapján lekéri az adott megrendelést
+    orderService.getOrder(orderId)
+      .then(orderdata => {
+          // eltárolja az adott megrendelés adatait
+          setOrder(orderdata);
+          // lekéri a megrendelésben levő termékek adatait
+          const orders = getCartList(orderdata.products).then(productsdata => setProducts(productsdata))
+          // lekéri az adott megrendelő adatait és eltárolja a nevét
+          userService.readUser(orderdata.uid)
+            .then(user => setUserName(user.name))         
+      })
+  }, [orderId])
+
+
 
   return (
     <>
@@ -14,25 +43,52 @@ export default function AdminOrderDetails() {
       <table className="ordertable">
         <thead>
           <tr>
-            <th>Megrendelt termék</th>
-            <th>Termék mennyisége</th>
-            <th>Részösszeg</th>
-            {/* <th>Vásárló neve</th>
-            <th>Részletek</th> */}
+            <th>időpont</th>
+            <th>megrendelés id</th>
+            <th>felhasználó id</th>
+            <th>felhasználó név</th>
           </tr>
         </thead>
         <tbody>
-          {displayedOrders.map(order => (
-            <tr key={order.id}>
-              <td>{order.id}</td>
-              <td>{order.timestamp}</td>
-              <td>{order.uid}</td>
-              {/* <td>{users[order.uid]?.name}</td>
-              <td>
-                <Link to={`/admin/megrendelesek/${order.id}/adatlap`}>Adatlap</Link>
-              </td> */}
-            </tr>
-          ))}
+          
+          <tr>
+            <td>{order.timestamp}</td>
+            <td>{order.id}</td>
+            <td>{order.uid}</td>
+            <td>{userName}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <table className="ordertable">
+        <thead>
+          <tr>
+            <th>Megrendelt termék</th>
+            <th>Megrendelt termék id</th>
+            <th>Termék mennyisége</th>
+            <th>Termék ára</th>
+            <th>Részösszeg</th>   
+          </tr>
+        </thead>
+        <tbody>
+          {
+            products?.map(p => (
+              <tr>
+                <td>{p.title}</td>
+                <td>{p.productId}</td>
+                <td>{p.amount}</td>
+                <td>{p.price}</td>
+                <td>{p.price * p.amount}</td>
+              </tr>
+            ))
+          }
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>végösszeg</td>
+            <td>{(products?.map(p => p.price * p.amount).reduce((sum, n) => n !== NaN ? sum + n : sum + 0, 0))}</td>
+          </tr>
         </tbody>
       </table>
       <Link className="admin-back-btn" to='/admin/megrendelesek'>Vissza</Link>
