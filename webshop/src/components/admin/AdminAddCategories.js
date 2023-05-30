e47b9cdcd7bb3991c1161b73945f1a925de999f6
kemenykin <kemenykin@gmail.com>
2023-05-30 07:52:17 +0200

    toast error and success

--- webshop/src/components/admin/AdminAddCategories.js
+++ webshop/src/components/admin/AdminAddCategories.js
@@ -9,37 +9,43 @@ export default function AdminAddCategories() {
     const [categoryName, setCategoryName] = useState("");
 
     function handleChange(e) {
-       setCategoryName(e.target.value)
+        setCategoryName(e.target.value)
     }
 
     function handleClick(e) {
         e.preventDefault()
-        categoryService.createCategory(categoryName);
-        toast.success("Sikeres kategória felvitel!", {
-            position: toast.POSITION.TOP_CENTER
-        });
+        if (!categoryName) {
+            toast.error("Kötelező megadni kategóriát!", {
+                position: toast.POSITION.TOP_CENTER
+            });
+        } else {
+            categoryService.createCategory(categoryName);
+            toast.success("Sikeres kategória felvitel!", {
+                position: toast.POSITION.TOP_CENTER
+            });
+        }
     }
-   
+
     return (
         <>
-        <h2 className= "adminaddcategories-h2">Termékkategória hozzáadása</h2>
-        <div className="add-categories">
-        <div className="add-categories-box">
-            <form>
-            <label htmlFor="name">Kategória név:</label>
-            <input 
-                type="text" 
-                name="name" 
-                value={categoryName.name} 
-                onChange={(e) => handleChange(e)} 
-            />
-            <br></br>
-            <button className="cat-button" onClick={(e) => handleClick(e)} type ="submit">Kategória létrehozása</button>
-            </form>
-            
+            <h2 className="adminaddcategories-h2">Termékkategória hozzáadása</h2>
+            <div className="add-categories">
+                <div className="add-categories-box">
+                    <form>
+                        <label htmlFor="name">Kategória név:</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={categoryName.name}
+                            onChange={(e) => handleChange(e)}
+                        />
+                        <br></br>
+                        <button className="cat-button" onClick={(e) => handleClick(e)} type="submit">Kategória létrehozása</button>
+                    </form>
+
+                </div>
             </div>
-        </div>
         </>
-        
+
     )
 }
