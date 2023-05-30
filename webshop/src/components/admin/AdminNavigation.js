c4ddcfea8ef74b0eaf1ae1f4be656a5f556d9ab4
livia-varga <varga.livia86@gmail.com>
2023-05-30 20:59:02 +0200

    fixing css

--- webshop/src/components/admin/AdminNavigation.js
+++ webshop/src/components/admin/AdminNavigation.js
@@ -15,12 +15,12 @@ const AdminNavigation = () => {
             <img className="logo1uj" src={logo1uj} alt="logo1uj" style={{ width: '150px'}}/>
                 <div className='navbar'>
                     <Link to='/admin' className="nav-link">Admin</Link>
-                    <Link to='/admin/termekek' className="nav-link">Terméklista</Link>
-                    <Link to='/admin/termek-felvitel' className="nav-link">Termékfelvitel</Link>
-                    <Link to='/admin/vasarlok' className="nav-link">Felhasználók lista</Link>
-                    <Link to='/admin/megrendelesek' className="nav-link">Megrendelések</Link>
                     <Link to='/admin/kategoriak' className="nav-link">Termék kategóriák</Link>
                     <Link to='/admin/kategoriak/uj-kategoria' className="nav-link">Kategória felvitel</Link>
+                    <Link to='/admin/termekek' className="nav-link">Terméklista</Link>
+                    <Link to='/admin/termek-felvitel' className="nav-link">Termékfelvitel</Link>
+                    <Link to='/admin/megrendelesek' className="nav-link">Megrendelések</Link>
+                    <Link to='/admin/vasarlok' className="nav-link">Felhasználók lista</Link>
                     <Link to='/'><button className="back-button" onClick={() => setAdmin(false)}><FaArrowLeft/>Vissza</button> </Link>
                 </div>              
             </div>
