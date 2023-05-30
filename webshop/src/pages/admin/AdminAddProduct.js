8d1d081bbbbd45c5ad673d46d6950cb488fcd467
kemenykin <kemenykin@gmail.com>
2023-05-31 01:01:05 +0200

    admin add product success toast added

--- webshop/src/pages/admin/AdminAddProduct.js
+++ webshop/src/pages/admin/AdminAddProduct.js
@@ -6,6 +6,8 @@ import '../../styles/adminsortsearch.css';
 import { fileUpload } from "../../utils/fileUpload";
 import readCategories from "../../service/categoryService";
 import categoryService from "../../service/categoryService";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function AdminAddProduct(props) {
 
@@ -66,8 +68,8 @@ export default function AdminAddProduct(props) {
         console.log(validateTitle());
         console.log(validatePrice());
         if (validateTitle() && validatePrice())
-        // 1. létrehozza a terméket firebase-n (url nélkül)
-        console.log("submit");
+            // 1. létrehozza a terméket firebase-n (url nélkül)
+            console.log("submit");
         productService.create(formData.title, formData.price, formData.categoryID)
             .then(data => {
                 // 2. feltölti a képet storage-be, feltöltéskor visszaadja a kép azonosítóját ('url'), beállítja useState-nek
@@ -75,40 +77,40 @@ export default function AdminAddProduct(props) {
                 // (harmadik lépés a fileUpload függvényen belül)
                 fileUpload(file, setUploadedUrl, data.id)
             })
-    
-
-        }
+        toast.success("Sikeres termék felvitel!", {
+            position: toast.POSITION.TOP_CENTER
+        });
+    }
 
 
     function validateTitle() {
-      	const title = formData.title;
-        if (title.match(/^\d+$/)) 
-        {
-          alert("A 'Terméknév' nem tartalmazhat csak számokat!");
-          return false;
-        }
-        
-      	if (title === "") {
-            alert("A 'Terméknév' nem lehet üres!");
-      		return false;
-        }
-        if (title.length < 2) {
-          alert("A 'Terméknév' minimum két karakter hosszú kell, hogy legyen!") 
-          return false;
+        const title = formData.title;
+        if (title.match(/^\d+$/)) {
+            alert("A 'Terméknév' nem tartalmazhat csak számokat!");
+            return false;
         }
 
-      	return true;
+        if (title === "") {
+            alert("A 'Terméknév' nem lehet üres!");
+            return false;
+        }
+        if (title.length < 2) {
+            alert("A 'Terméknév' minimum két karakter hosszú kell, hogy legyen!")
+            return false;
+        }
+
+        return true;
     }
 
     function validatePrice() {
-      const price = formData.price;
+        const price = formData.price;
         if (isNaN(price)) {
-          alert("Az 'Ár' csak számokat tartalmazhat!");
-        	return false;
+            alert("Az 'Ár' csak számokat tartalmazhat!");
+            return false;
         }
         if (price === "") {
-          alert("Az 'Ár' nem lehet üres!");
-          return false;
+            alert("Az 'Ár' nem lehet üres!");
+            return false;
         }
         return true
     }
@@ -125,66 +127,66 @@ export default function AdminAddProduct(props) {
     }
 
     return (
-        
-        <div className="add-product">  
-        <h2 className="adminaddproduct-h2">Termékfelvitel</h2>
-        <div className="box-container">
-        <div className="box">
-            <label>Új termék adatai: </label>
-            <div className="inside-box">
-            <label htmlFor="title">Terméknév:</label>
-            <input
-                type="text"
-                name="title"
-                value={formData.title}
-                onChange={(e) => updateTitle(e)}
-            />
-            <br />
-            <label htmlFor="price">Ár: </label>
-            <input
-                type="text"
-                name="price"
-                value={formData.price}
-                onChange={(e) => updatePrice(e)}
-            />
-            </div>
-            </div>
+
+        <div className="add-product">
+            <h2 className="adminaddproduct-h2">Termékfelvitel</h2>
+            <div className="box-container">
+                <div className="box">
+                    <label>Új termék adatai: </label>
+                    <div className="inside-box">
+                        <label htmlFor="title">Terméknév:</label>
+                        <input
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={(e) => updateTitle(e)}
+                        />
+                        <br />
+                        <label htmlFor="price">Ár: </label>
+                        <input
+                            type="text"
+                            name="price"
+                            value={formData.price}
+                            onChange={(e) => updatePrice(e)}
+                        />
+                    </div>
+                </div>
             </div>
             <br />
             <div className="box-container">
-            <div className="box">
-            <label>Kategória kiválasztása: </label>
-            <div className="inside-box">
-            <div className="select-option">
-            <select value={formData.categoryID} id="categories-list" onChange={(e) => updateCategory(e)} >
-                <option value="">Válassz egy kategóriát!</option>
-                {Object.values(categoryData).map(cat => <option value={cat.id}>{cat.name}</option>)}
-            </select>
-            </div>
-            </div>
-            </div>
+                <div className="box">
+                    <label>Kategória kiválasztása: </label>
+                    <div className="inside-box">
+                        <div className="select-option">
+                            <select value={formData.categoryID} id="categories-list" onChange={(e) => updateCategory(e)} >
+                                <option value="">Válassz egy kategóriát!</option>
+                                {Object.values(categoryData).map(cat => <option value={cat.id}>{cat.name}</option>)}
+                            </select>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <br />
             <div className="box-container">
-            <div className="box">
-            <label htmlFor="img">Kép feltöltése a termékhez: </label>
-            <div className="inside-box">
-                <label className="img-upload-label" htmlFor="img">Képfeltöltés</label>
-            <input
-                type="file"
-                name="img"
-                id="img"
-                onChange={(e) => handleImgUpload(e)}
-            />
+                <div className="box">
+                    <label htmlFor="img">Kép feltöltése a termékhez: </label>
+                    <div className="inside-box">
+                        <label className="img-upload-label" htmlFor="img">Képfeltöltés</label>
+                        <input
+                            type="file"
+                            name="img"
+                            id="img"
+                            onChange={(e) => handleImgUpload(e)}
+                        />
 
-            <div className="uploaded-img">
-                {file &&
-                    <><p>termék kép: </p><img src={previewImg} alt="" style={{ width: "300px" }} /></>}
-            </div>
-            </div>
-            <br />
-            </div>
+                        <div className="uploaded-img">
+                            {file &&
+                                <><p>termék kép: </p><img src={previewImg} alt="" style={{ width: "300px" }} /></>}
+                        </div>
+                    </div>
+                    <br />
+                </div>
             </div>
             <button className="addp-button" onClick={onSubmit}>Termék hozzáadása</button>
         </div>
