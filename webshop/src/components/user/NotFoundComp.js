d6f49502af24665a470858da1b4339f8ad112984
kemenykin <kemenykin@gmail.com>
2023-05-30 09:58:01 +0200

    className added to container

--- webshop/src/components/user/NotFoundComp.js
+++ webshop/src/components/user/NotFoundComp.js
@@ -2,7 +2,7 @@ import '../../styles/notfound.css';
 
 const NotFoundComp = () => {
     return (
-        <div>
+        <div className="notfound-container">
             <h1>Hoppá! Eltévedtél?</h1>
             <p>Az általad keresett oldal nem létezik.</p>
             <img src="https://imagizer.imageshack.com/img923/7401/V44Qqu.png" alt="" style={{width: "650px"}}/>
