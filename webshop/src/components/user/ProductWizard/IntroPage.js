b24a3331b14c33423afc07480398698ab230a16a
kemenykin <kemenykin@gmail.com>
2023-05-30 16:34:21 +0200

    wizard intro page img size corrected

--- webshop/src/components/user/ProductWizard/IntroPage.js
+++ webshop/src/components/user/ProductWizard/IntroPage.js
@@ -6,7 +6,7 @@ const IntroPage = () => {
     return (
         <section className="wizard-intro">
             <h2>Termék varázsló</h2>
-            <p><img src="https://images.pexels.com/photos/6707334/pexels-photo-6707334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /></p>
+            <p><img src="https://images.pexels.com/photos/6707334/pexels-photo-6707334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{width: "400px"}} /></p>
             <div className="intro-container">
                 <p>
                     Mit szólnál hozzá, ha egy gyors és interaktív kérdőív segítségével segítetnénk
